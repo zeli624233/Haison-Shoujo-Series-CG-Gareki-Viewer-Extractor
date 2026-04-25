ba63eb34f4124e1dcb43df0d0a66adfc4d808a09
ユイ可愛ね <2752630240@qq.com>
2026-04-25 21:48:48 +0800

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 cd "$(dirname "$0")"
 python -m pip install -r requirements.txt
 python -m pip install -r requirements-build.txt
-python -m PyInstaller HaisouShoujoViewerExtractor.spec --clean --noconfirm
+python -m PyInstaller HaisonShoujoViewerExtractor.spec --clean --noconfirm
